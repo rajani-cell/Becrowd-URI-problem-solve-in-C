0f842d97d5712fc14621b38f2ca351637f65d28d
Rajani Akther <151865891+rajani-cell@users.noreply.github.com>
2023-12-20 00:44:23 +0600

    Create 1015.c
    Done

---
+++ 1015.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+
+
+ 
+int main() {
+ 
+ double x1,y1;
+ scanf("%lf%lf",&x1,&y1);
+ 
+ double x2,y2;
+ scanf("%lf%lf",&x2,&y2);
+ 
+ double z=((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+ 
+ double ans = sqrt(z);
+ 
+ printf("%.4lf\n",ans);
+ 
+    return 0;
+}
