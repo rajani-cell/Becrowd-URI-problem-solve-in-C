206711d48405d3f81144dc775dee44388f8679e3
Rajani Akther <151865891+rajani-cell@users.noreply.github.com>
2023-12-19 18:45:04 +0600

    Create 1011.c
    Done

---
+++ 1011.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   double R,pi=3.14159;
+   scanf("%lf",&R);
+   
+   double VOLUME=(4.0/3)*pi*R*R*R;
+   
+   printf("VOLUME = %.3lf\n",VOLUME);
+   
+ 
+    return 0;
+}
